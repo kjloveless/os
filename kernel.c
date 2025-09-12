7c5a00dca011e9775be9a8cd9f47e9064f54e4aa
Kyle <kjloveless321@gmail.com>
2025-09-12 13:50:22 -0500

    add process scheduler

--- kernel.c
+++ kernel.c
@@ -8,6 +8,16 @@ typedef uint32_t size_t;
 extern char __bss[], __bss_end[], __stack_top[];
 extern char __free_ram[], __free_ram_end[];
 
+__attribute__((naked)) void switch_context(uint32_t *prev_sp, uint32_t *next_sp);
+struct process *create_process(uint32_t pc);
+void yield(void);
+
+
+struct process procs[PROCS_MAX];    // all process control structures
+struct process *current_proc;       // currently running process
+struct process *idle_proc;          // idle proc
+
+
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
     long arg5, long fid, long eid) {
   register long a0 __asm__("a0") = arg0;
@@ -25,6 +35,27 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
         "r"(a6), "r"(a7)
       : "memory");
   return (struct sbiret){.error = a0, .value = a1};
+}
+
+void yield(void) {
+  // search for a runnable process
+  struct process *next = idle_proc;
+  for (int i = 0; i < PROCS_MAX; i++) {
+    struct process *proc = &procs[(current_proc->pid + i) % PROCS_MAX];
+    if (proc->state == PROC_RUNNABLE && proc->pid > 0) {
+      next = proc;
+      break;
+    }
+  }
+
+  // if there's no runnable process other than the current one, return and continue processing
+  if (next == current_proc)
+    return;
+
+  // context switch
+  struct process *prev = current_proc;
+  current_proc = next;
+  switch_context(&prev->sp, &next->sp);
 }
 
 paddr_t alloc_pages(uint32_t n) {
@@ -52,6 +83,85 @@ void boot(void) {
     :
     : [stack_top] "r" (__stack_top) // Pass the stack top address as %[stack_top]
   );
+}
+
+struct process *create_process(uint32_t pc) {
+  // find an unused process control structure
+  struct process *proc = NULL;
+  int i;
+  for (i = 0; i  < PROCS_MAX; i++) {
+    if (procs[i].state == PROC_UNUSED) {
+      proc = &procs[i];
+      break;
+    }
+  }
+
+  if (!proc)
+    PANIC("no free process slots");
+
+  // stack callee-saved registers. these register values will be restored in
+  // the first context switch in switch_context
+  uint32_t *sp = (uint32_t *) &proc->stack[sizeof(proc->stack)];
+  *--sp = 0;                      // s11
+  *--sp = 0;                      // s10
+  *--sp = 0;                      // s9
+  *--sp = 0;                      // s8
+  *--sp = 0;                      // s7
+  *--sp = 0;                      // s6
+  *--sp = 0;                      // s5
+  *--sp = 0;                      // s4
+  *--sp = 0;                      // s3
+  *--sp = 0;                      // s2
+  *--sp = 0;                      // s1
+  *--sp = 0;                      // s0
+  *--sp = (uint32_t) pc;          // ra
+                                  
+  // initialize fields
+  proc->pid = i + 1;
+  proc->state = PROC_RUNNABLE;
+  proc->sp = (uint32_t) sp;
+  return proc;
+}
+
+__attribute__((naked)) void switch_context(uint32_t *prev_sp, uint32_t *next_sp) {
+  __asm__ __volatile__(
+      // save callee-saved registers onto the current process's stack
+      "addi sp, sp, -13 * 4\n"      // allocate stack space for 13 4-byte registers
+      "sw ra, 0 * 4(sp)\n"          // save callee-saved registers only
+      "sw s0, 1 * 4(sp)\n"
+      "sw s1, 2 * 4(sp)\n"
+      "sw s2, 3 * 4(sp)\n"
+      "sw s3, 4 * 4(sp)\n"
+      "sw s4, 5 * 4(sp)\n"
+      "sw s5, 6 * 4(sp)\n"
+      "sw s6, 7 * 4(sp)\n"
+      "sw s7, 8 * 4(sp)\n"
+      "sw s8, 9 * 4(sp)\n"
+      "sw s9, 10 * 4(sp)\n"
+      "sw s10, 11 * 4(sp)\n"
+      "sw s11, 12 * 4(sp)\n"
+
+      // switch the stack pointer
+      "sw sp, (a0)\n"       // *prev_sp = sp
+      "lw sp, (a1)\n"       // switch stack pointer (sp) here
+                            
+      // restore callee-saved registers from the next process's stack
+      "lw ra, 0 * 4(sp)\n"      // restore callee-saved registers only
+      "lw s0, 1 * 4(sp)\n"
+      "lw s1, 2 * 4(sp)\n"
+      "lw s2, 3 * 4(sp)\n"
+      "lw s3, 4 * 4(sp)\n"
+      "lw s4, 5 * 4(sp)\n"
+      "lw s5, 6 * 4(sp)\n"
+      "lw s6, 7 * 4(sp)\n"
+      "lw s7, 8 * 4(sp)\n"
+      "lw s8, 9 * 4(sp)\n"
+      "lw s9, 10 * 4(sp)\n"
+      "lw s10, 11 * 4(sp)\n"
+      "lw s11, 12 * 4(sp)\n"
+      "addi sp, sp, 13 * 4\n" // we've popped 13 4-byte registers from the stack
+      "ret\n"
+    );
 }
 
 __attribute__((naked))
@@ -140,15 +250,49 @@ void handle_trap(struct trap_frame *f) {
   PANIC("unexpected trap scause=%x, stval=%x, sepc=%x\n", scause, stval, user_pc);
 }
 
+void delay(void) {
+  for (int i = 0; i < 30000000; i++)
+    __asm__ __volatile__("nop");  // do nothing
+}
+
+struct process *proc_a;
+struct process *proc_b;
+
+void proc_a_entry(void) {
+  printf("starting process a\n");
+  while (1) {
+    putchar('a');
+    yield();
+    delay();
+  }
+}
+
+void proc_b_entry(void) {
+  printf("starting process b\n");
+  while (1) {
+    putchar('b');
+    yield();
+    delay();
+  }
+}
+
 void kernel_main(void) {
   memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
-  paddr_t paddr0 = alloc_pages(2);
-  paddr_t paddr1 = alloc_pages(1);
-  printf("alloc_pages test: paddr0=%x\n", paddr0);
-  printf("alloc_pages test: paddr1=%x\n", paddr1);
+  printf("\n\n");
 
-  PANIC("booted");
+  WRITE_CSR(stvec, (uint32_t) kernel_entry);
+
+  idle_proc = create_process((uint32_t) NULL);
+  idle_proc->pid = 0;   // idle
+  current_proc = idle_proc;
+
+  proc_a = create_process((uint32_t) proc_a_entry);
+  proc_b = create_process((uint32_t) proc_b_entry);
+
+  yield();
+
+  PANIC("switched to idle process");
   printf("unreachable\n");
 
   for (;;) {
@@ -156,10 +300,3 @@ void kernel_main(void) {
   }
 }
 
-__attribute__((naked)) void switch_context(uint32_t *prev_sp, uint32_t *next_sp) {
-  __asm__ __volatile__(
-    // save callee-saved registers onto the current process's stack
-    "addi sp, sp, -13 * 4\n"  // allocate stack space for 13 4-byte registers
-    "sw ra, 0 * 4(sp)\n"      // save callee-saved registers only
-    "sw s0, 1 * 4(sp)\n"
-}
